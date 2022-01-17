39385d24edf6e1f691e968d1faa40272fb3a9f62
shailendra kumar <shailendraftp0001@gamil.com>
2022-01-17 16:50:22 +0530

    added

---
+++ components/sidebar.js
@@ -0,0 +1,16 @@
+function sidebar() {
+    
+  return `  <div id="box">
+  <h1>Techcrunch</h1>
+  <div><button>login</button></div>
+  <div><input type="text" placeholder="Search news" id="news"></div>
+  <div>Startup</div>
+  <div>Newslaters</div>
+  <div>Audio</div>
+  <div>Video</div>
+</div>`
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+}
+export default sidebar
