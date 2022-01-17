c987e9ab509e690b09069fe6a345dd028149751a
Shailendra Kumar <93985081+shailendra7518@users.noreply.github.com>
2022-01-17 22:51:57 +0530

    all

---
+++ components/sidebar.js
@@ -0,0 +1,17 @@
+function sidebar() {
+
+  return `  <div id="box">
+  <h1>Techcrunch</h1>
+  <div><button id="but">search</button></div>
+  <div><input type="text" placeholder="Search news" id="news"></div>
+  <div>Startup</div>
+  <div>Newslaters</div>
+  <div>Audio</div>
+  <div>Video</div>
+  <button id="call1">puther</button>
+</div>`
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+}
+export default sidebar
---
+++ scripts/search.js
@@ -0,0 +1,6 @@
+function storeSearchterm(term) {
+    localStorage.setItem("article",JSON.stringify(term))
+
+}
+
+export default storeSearchterm
